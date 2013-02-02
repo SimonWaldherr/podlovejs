8665809c5ab396adbc64bbe3f61da3b726849dfe
gerritvanaaken <gerrit@praegnanz.de>
2013-02-02 23:17:11 +0100

    even more settings page

--- settings.php
+++ settings.php
@@ -28,11 +28,10 @@ function podlovewebplayer_create_menu() {
 }
 
 function podlovewebplayer_register_settings() {
-  register_setting( 'podlovewebplayer_options', 'podlovewebplayer_options');
-
+  register_setting( 'podlovewebplayer_options', 'podlovewebplayer_options' );
   add_settings_section( 'podlovewebplayer_enclosures', 'Enclosures', false, 'podlovewebplayer' );
   add_settings_field( 
-    'podlovewebplayer_enclosure_detect', 
+    'podlovewebplayer_enclosure_detect',
     'Turn enclosures to players',
     function(){ 
       $options = get_option('podlovewebplayer_options');
@@ -40,11 +39,26 @@ function podlovewebplayer_register_settings() {
       if ( isset( $options['enclosure_detect'] ) )
         $checked = "checked ";
       print "<input id='pwpenclosures1' name='podlovewebplayer_options[enclosure_detect]' 
-        $checked type='checkbox' value='1' />";
-    }, 'podlovewebplayer', 'podlovewebplayer_enclosures', array( 'label_for' => 'pwpenclosures1' ));
+        $checked type='checkbox' value='1' />&nbsp;&nbsp;
+        (WordPress automatically creates an \"enclosure\" custom field whenever it detects an URL to a media file in the post text. 
+      Use this option to turn these enclosures into Podlove Web Player instances.)";
+    }, 'podlovewebplayer', 'podlovewebplayer_enclosures', array( 'label_for' => 'pwpenclosures1' )
+  );
+  add_settings_field( 
+    'podlovewebplayer_enclosure_force',
+    'Force enclosure players',
+    function(){ 
+      $options = get_option('podlovewebplayer_options');
+      $checked = "";
+      if ( isset( $options['enclosure_force'] ) )
+        $checked = "checked ";
+      print "<input id='pwpenclosures1' name='podlovewebplayer_options[enclosure_force]' 
+        $checked type='checkbox' value='1' />&nbsp;&nbsp;
+        (additionally to regular Podlove Web Players, if both are present)";
+    }, 'podlovewebplayer', 'podlovewebplayer_enclosures', array( 'label_for' => 'pwpenclosures2' )
+  );
   
   /*
-  add_settings_field( 'podlovewebplayer_enclosures', 'pwp_enclosure_force' );
   add_settings_field( 'podlovewebplayer_enclosures', 'pwp_enclosure_bottom' );
   */
 
